9fb3fed5ff07173d5f60c1be0a16cb6163d80744
Jean Lancer <jeanlancerti@gmail.com>
2020-06-10 20:36:46 -0300

    final commit

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -6,6 +6,7 @@ import IProductsRepository from '@modules/products/repositories/IProductsReposit
 import ICustomersRepository from '@modules/customers/repositories/ICustomersRepository';
 import Order from '../infra/typeorm/entities/Order';
 import IOrdersRepository from '../repositories/IOrdersRepository';
+import ICreateOrderDTO from '../dtos/ICreateOrderDTO';
 
 interface IProduct {
   id: string;
@@ -20,13 +21,47 @@ interface IRequest {
 @injectable()
 class CreateOrderService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
     // TODO
+    const customer = await this.customersRepository.findById(customer_id);
+
+    if (typeof customer === 'undefined') {
+      throw new AppError('Customer not found');
+    }
+
+    const foundProducts = await this.productsRepository.findAllById(products);
+
+    if (foundProducts.length < products.length) {
+      throw new AppError('Products not found');
+    }
+
+    const dataOrder: ICreateOrderDTO = {
+      customer,
+      products: foundProducts.map((product, index) => {
+        if (product.quantity < products[index].quantity) {
+          throw new AppError('Out of stock');
+        }
+
+        return {
+          product_id: product.id,
+          price: product.price,
+          quantity: products[index].quantity,
+        };
+      }),
+    };
+
+    const order = await this.ordersRepository.create(dataOrder);
+    await this.productsRepository.updateQuantity(products);
+
+    return order;
   }
 }
 
